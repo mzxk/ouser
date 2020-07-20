0da37fb3a02f413d2d2b928fa43da55dfeb718a9
nono <catjuicy@gmail.com>
2020-07-21 01:07:24 +0800

    增加了一个实用函数 可以将几个字段用.链接

--- utils.go
+++ utils.go
@@ -3,9 +3,13 @@ package ouser
 import (
 	"crypto/sha512"
 	"encoding/hex"
+	"strings"
 )
 
 func sha(s string) string {
 	k := sha512.Sum512([]byte(s))
 	return hex.EncodeToString(k[:])[58:98]
+}
+func getType(s ...string) string {
+	return strings.Join(s, ".")
 }
