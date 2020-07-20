745b57853946f296e620dae7a9ad20322f16f328
nono <catjuicy@gmail.com>
2020-07-21 01:06:58 +0800

    现在唯一索引的字段，保存到数据库可以为空

--- stru.go
+++ stru.go
@@ -4,17 +4,17 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `bson:"_id"`
-	User     string             //用户名
-	Pwd      string             `json:"-"` //密码s
-	RegIP    string             `json:"-"` //注册ip
-	Referrer string             `json:"-"` //推荐人
+	User     string             `bson:"user,omitempty"` //用户名
+	Pwd      string             `json:"-"`              //密码s
+	RegIP    string             `json:"-"`              //注册ip
+	Referrer string             `json:"-"`              //推荐人
 
 	NickName        string //用户自定义昵称
 	Avatar          string //头像ID
 	DeliveryAddress string //用户实际地址
 
-	Email string //用户email
-	Phone string //用户手机号码
+	Email string `bson:"email,omitempty"` //用户email
+	Phone string `bson:"phone,omitempty"` //用户手机号码
 
 	RealName string //用户真名
 	Verify   int64  //用户认证等级
