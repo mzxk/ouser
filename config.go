38c3f4c765e9f5e0e1e584dfcc4f6947a22d5611
nono <catjuicy@gmail.com>
2020-08-02 23:43:15 +0800

    初始化改成独立的文件

--- config.go
+++ config.go
@@ -1,13 +1,5 @@
 package ouser
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-
-	"github.com/mzxk/omongo"
-)
-
 type Config struct {
 	Name       string
 	MongoURL   string
@@ -31,37 +23,3 @@ type Config struct {
 }
 
 var cfg *Config
-
-func Init() {
-	bt, err := ioutil.ReadFile("userConfig.json")
-	if err != nil {
-		cfg = &Config{
-			MongoURL: "mongodb://127.0.0.1:27017",
-			RedisURL: "127.0.0.1:6379",
-		}
-		cfg.Register.Limit = 5
-		cfg.Login.Limit = 5
-
-		js, _ := json.Marshal(cfg)
-		_ = ioutil.WriteFile("userConfig.json", js, 0777)
-		panic(err)
-	}
-	var cage Config
-	err = json.Unmarshal(bt, &cage)
-	if err != nil {
-		panic(err)
-	}
-	cfg = &cage
-	createIndex(cfg.MongoURL)
-}
-func createIndex(s string) {
-	mgo := omongo.NewMongoDB(s, "user")
-	defer mgo.MgoClient.Disconnect(nil)
-	log.Println("Start to ensure users index")
-	err := mgo.CreateIndexes("user", "user", []string{"u_user_1", "u_phone_1", "u_email_1"})
-	err = mgo.CreateIndexes("user", "feedback", []string{"u_bid_1"})
-	if err != nil {
-		panic(err)
-	}
-	log.Println("End ensure users index")
-}
---
+++ init.go
@@ -0,0 +1,73 @@
+package ouser
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"github.com/mzxk/obalance"
+	"github.com/mzxk/ohttp"
+	"github.com/mzxk/omongo"
+	"github.com/mzxk/oval"
+)
+
+var userCache = oval.NewExpire() //用户信息的缓存
+
+type Ouser struct {
+	mgo        *omongo.MongoDB
+	httpClient *ohttp.Server
+	sms        ohttp.Sms
+	balance    *obalance.Balance
+}
+
+func New(clt *ohttp.Server) *Ouser {
+	Init()
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	t := &Ouser{
+		mgo:        omongo.NewMongoDB(cfg.MongoURL, "user"),
+		httpClient: clt,
+		sms:        ohttp.NewSms(cfg.Sms.Name, cfg.Sms.Key),
+	}
+	if cfg.BalanceURL != "" {
+		t.balance = obalance.NewRemote(cfg.BalanceURL)
+	} else {
+		t.balance = obalance.NewLocal(cfg.RedisURL, cfg.RedisPwd)
+	}
+	return t
+}
+func Init() {
+	bt, err := ioutil.ReadFile("userConfig.json")
+	if err != nil {
+		cfg = &Config{
+			MongoURL: "mongodb://127.0.0.1:27017",
+			RedisURL: "127.0.0.1:6379",
+		}
+		cfg.Register.Limit = 5
+		cfg.Login.Limit = 5
+
+		js, _ := json.Marshal(cfg)
+		_ = ioutil.WriteFile("userConfig.json", js, 0777)
+		panic(err)
+	}
+	var cage Config
+	err = json.Unmarshal(bt, &cage)
+	if err != nil {
+		panic(err)
+	}
+	cfg = &cage
+	createIndex(cfg.MongoURL)
+}
+func createIndex(s string) {
+	mgo := omongo.NewMongoDB(s, "user")
+	defer mgo.MgoClient.Disconnect(nil)
+	log.Println("Start to ensure users index")
+	err := mgo.CreateIndexes("user", "user", []string{"u_user_1", "u_phone_1", "u_email_1"})
+	err = mgo.CreateIndexes("user", "feedback", []string{"u_bid_1"})
+	err = mgo.CreateIndexes("user", "shoplist", []string{"bid_1"})
+	err = mgo.CreateIndexes("user", "shopitems", []string{"ban_1"})
+	err = mgo.CreateIndexes("user", "withdraw", []string{"bid_1"})
+	if err != nil {
+		panic(err)
+	}
+	log.Println("End ensure users index")
+}
