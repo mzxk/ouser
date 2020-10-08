6dedc7f01b9a181762225ac8dc2e6b2acde28372
nono <catjuicy@gmail.com>
2020-10-08 23:28:21 +0800

    实装了多币种，并在服务器错误时，提示服务未开放

--- address.go
+++ address.go
@@ -1,29 +1,44 @@
 package ouser
 
-import "github.com/mzxk/ohttp"
+import (
+	"log"
+
+	"github.com/mzxk/ohttp"
+)
 
 func (t *Ouser) AddressGet(p map[string]string) (interface{}, error) {
+
 	uid := p["bsonid"]
-	//TODO 未实装多币种
-	//coin := p["coin"]
-	//if coin == "usdt" {
-	//
-	//}
+	currency := p["currency"]
+	if currency == "" {
+		return nil, errs("WrongParams")
+	}
+	if cfg.AddressURL == "" {
+		return [][]string{{currency, "服务未开放"}}, nil
+	}
+	chains := []string{currency}
+	if currency == "usdt" {
+		chains = []string{"eth", "trx"}
+	}
+	var result [][]string
+	for _, v := range chains {
+		result = append(result, []string{v, t.addressGet(cfg.Name, uid, v)})
+	}
+	return result, nil
+}
+func (t *Ouser) addressGet(web, uid, chain string) string {
 	var result string
 	rsp, err := ohttp.HTTP(cfg.AddressURL, map[string]interface{}{
-		"web":   cfg.Name,
+		"web":   web,
 		"user":  uid,
-		"chain": "eth",
+		"chain": chain,
 	}).Get()
 	if err != nil {
-		return nil, err
+		return "服务未开放"
 	}
 	err = rsp.JSONSelf(&result)
 	if err != nil {
-		return nil, err
+		log.Println("ERROR Get Address", err)
 	}
-
-	return [][]string{
-		{"eth", result},
-	}, nil
+	return result
 }
